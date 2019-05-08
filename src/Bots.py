b41a5a4eff0d96a79f48af31c5e650f55782be85
waltsworld <calabastas@gmail.com>
2019-05-07 20:36:00 -0600

    CR Additions

--- src/Bots.py
+++ src/Bots.py
@@ -1,25 +1,26 @@
+
 # __init__()
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout, Flatten, TimeDistributed
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense, Activation, Dropout, Flatten 
 from keras.optimizers import Adam
 from keras.models import load_model
 # LSTM __init__()
-from keras.layers import LSTM
-from keras import regularizers
+
 # save()
 from os.path import isdir
 from time import localtime, strftime
 # remember()
 from collections import deque
 import random
+
 '''Class Bot
 A Basic ANN that inherits from Keras Sequential and extends for reinforcement learning and
 hindsight experience replay support. 
 '''
 class Bot(Sequential):
     '''See help(Sequential). Inheritance here from Keras Sequential()
+    Notes: The ANN wants unwound data. For windowed state, it unwraps into a single instance.
     Parameters:
         NN_input_shape()
             This tuple must be compatible with the Sequential() input layer input_shape. 
@@ -33,6 +34,7 @@ class Bot(Sequential):
             Using a batch_size, returns a collection of memories
     Returns: Instance of Sequential() with additional methods 
     '''
+
     def __init__(self, NN_input_shape, action_space=3, weights_filename=None, verbose=True):
         # Params
         self.NN_input_shape = NN_input_shape
@@ -52,7 +54,7 @@ class Bot(Sequential):
         print('Creating Model...')
         # This inherits from Sequential.
         super().__init__()
-        self.add(Dense(units=64, input_shape=self.NN_input_shape, activation="relu", kernel_regularizer=regularizers.l2(0.01)))
+        self.add(Dense(units=64, input_shape=self.NN_input_shape, activation="relu"))
         self.add(Dense(units=32, activation="relu"))
         self.add(Dense(units=8, activation="relu"))
         self.add(Dense(self.action_space, activation="linear"))
@@ -97,6 +99,7 @@ hindsight experience replay support.
 '''
 class Bot_LSTM(Sequential):
     '''See help(Sequential). Inheritance here from Keras Sequential()
+    Notes: Linear, MSE, Adam(lr=very low) would be used for RL
     Parameters:
         NN_input_shape()
             This tuple must be compatible with the Sequential() input layer input_shape. For LSTMs 
@@ -112,6 +115,9 @@ class Bot_LSTM(Sequential):
             Using a batch_size, returns a collection of memories
     Returns: Instance of Sequential() with additional methods 
     '''
+
+    from keras.layers import LSTM
+    
     def __init__(self, NN_input_shape, action_space=3, weights_filename=None, verbose=True):
         # Params
         self.NN_input_shape = NN_input_shape
@@ -170,11 +176,11 @@ class Bot_LSTM(Sequential):
 
 '''Class Bot_CNN_LSTM
 A basic CNN to RNN with LSTM bot that inherits from Keras Sequential and extends 
-for reinforcement learning and
-hindsight experience replay support. 
+for reinforcement learning and hindsight experience replay support. 
 '''
 class Bot_CNN_LSTM(Sequential): # Didn't get there yet
     '''See help(Sequential). Inheritance here from Keras Sequential()
+    Notes: The CNNs want 2D data. Being in TD wrapper, need "features" wide by single tall input shape. 
     Parameters:
         NN_input_shape()
             This tuple must be compatible with the Sequential() input layer input_shape. For LSTMs 
@@ -190,6 +196,7 @@ class Bot_CNN_LSTM(Sequential): # Didn't get there yet
             Using a batch_size, returns a collection of memories
     Returns: Instance of Sequential() with additional methods 
     '''
+    from keras.layers import LSTM, Conv2D, MaxPooling2D, TimeDistributed
     def __init__(self, NN_input_shape, action_space=3, weights_filename=None, verbose=True):
         # Params
         self.NN_input_shape = NN_input_shape
@@ -211,8 +218,8 @@ class Bot_CNN_LSTM(Sequential): # Didn't get there yet
         # This inherits from Sequential.
         super().__init__()
         self.add(Activation('tanh', input_shape=self.NN_input_shape))
-        self.add(TimeDistributed(Conv2D(12, kernel_size=(3, 7), activation='relu')))
-        self.add(TimeDistributed(MaxPooling2D(pool_size=(3, 7))))
+        self.add(TimeDistributed(Conv2D(12, kernel_size=(1, self.NN_input_shape[0]), activation='relu')))
+        self.add(TimeDistributed(MaxPooling2D(pool_size=(1, self.NN_input_shape[0]))))
         self.add(TimeDistributed(Flatten()))
         self.add(LSTM(units=8, return_sequences = True, kernel_regularizer=regularizers.l2(0.01)))
         self.add(LSTM(units=8, kernel_regularizer=regularizers.l2(0.01)))
